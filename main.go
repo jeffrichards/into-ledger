cd91e0265a990677a6e95fddc8b751ad721e46ff
Jeff Richards <jrichards@revenuewire.com>
2023-07-17 08:21:16 -0700

    allow for poorly coded csv files.

--- main.go
+++ main.go
@@ -228,6 +228,7 @@ func printCategory(t Txn) {
 	if len(cat) > catLength {
 		cat = cat[len(cat)-catLength:]
 	}
+	color.New(color.BgGreen, color.FgBlack).Printf(" %6s %-20s ", prefix, cat)
 }
 
 func printSummary(t Txn, idx, total int) {
